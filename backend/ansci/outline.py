c1ea84d2f49b8932b105ef43c85bf0e8de75694a
Pushkar Nimkar <pushkarnim@gmail.com>
2025-06-22 01:37:27 -0700

    Adding workflow file

--- backend/ansci/outline.py
+++ backend/ansci/outline.py
@@ -6,4 +6,4 @@ def generate_outline(history: list[dict]) -> AnsciOutline | None:
     Generate an outline for the animation from message history. We make sure
     that the history contains the paper as a file, and user messages.
     """
-    raise NotImplementedError("Not implemented")
+    return None
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
+from io import BytesIO
 import argparse
+from ansci.workflow import create_animation
 
 
-def main(paper_path: str, output_path: str):
+def main(paper_path: str, output_path: str, prompt: str | None = None):
+    with open(paper_path, "rb") as paper_file:
+        paper_bytes = BytesIO(paper_file.read())
+        create_animation(paper_bytes, output_path, prompt)
     print(f"Hello from server! {paper_path} {output_path}")
 
 
@@ -9,6 +14,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--paper", type=str, required=True)
     parser.add_argument("--output", type=str, required=True)
-    parser.add_argument("--prompt", type=str, required=True)
+    parser.add_argument("--prompt", type=str)
     args = parser.parse_args()
-    main(args.paper, args.output)
+    main(args.paper, args.output, args.prompt)
